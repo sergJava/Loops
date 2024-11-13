b965d636acbefd77904c20360ca4c612da79ebfa
sergFibih <sergfibih@ya.ru>
2024-11-13 18:26:20 +1000

    урок 2

--- src/Main.java
+++ src/Main.java
@@ -2,66 +2,115 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Task 1");
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + "\t");
+
+        float accumulation = 0.0f;
+        int month = 1;
+        while (accumulation < 2_459_000) {
+            accumulation = (float) (accumulation * 1.01 + 15_000);
+            System.out.printf("Месяц %d, сумма накоплений равна %.2f рублей\n", month, accumulation);
+            month++;
         }
-        System.out.print("\n\n");
+        System.out.println("\n");
+
 
         System.out.println("Task 2");
-        for (int i = 10; i >= 1; i--) {
-            System.out.print(i + "\t");
+
+        int count = 1;
+        while (count <= 10) {
+            System.out.print(count + " ");
+            count++;
         }
-        System.out.print("\n\n");
+        System.out.println();
+        for (; count >= 1; count--) {
+            if (count > 10) {
+                continue;
+            }
+            System.out.print(count + " ");
+        }
+        System.out.println("\n");
 
         System.out.println("Task 3");
-        for (int i = 0; i <= 17; i+=2) {
-            System.out.print(i + "\t");
-        }
-        System.out.print("\n\n");
 
-        System.out.println("Task 4" + "\t");
-        for (int i = 10; i >=-10 ; i--) {
-            System.out.print(i + "\t");
+        int people = 12_000_000;
+        float fertility = (float) 17 / 1000;
+        float mortality = (float) 8 / 1000;
+        float different = fertility - mortality;
+        for (int year = 1; year <= 10; year++) {
+            people = people + (int)(people * different);
+            System.out.printf("Год %d, численность населения составляет %d\n", year, people);
         }
-        System.out.print("\n\n");
+        System.out.println("\n");
+
+
+        System.out.println("Task 4");
+
+        float accumulation4 = 15_000.0f;
+        int month4 = 1;
+        while (accumulation4 < 12_000_000.0f) {
+            accumulation4 *= 1.07;
+            System.out.printf("Месяц %d, сумма вклада %.2f\n", month4, accumulation4);
+            if (accumulation4 > 12_000_000) {
+                continue;
+            }
+            month4++;
+        }
+        System.out.printf("\nВасилию нужно %d месяцев чтобы накопить больше 12 000 000 рублей\n\n", month4);
+
 
         System.out.println("Task 5");
-        for (int i = 1904; i <= 2096; i+=4) {
-            System.out.println(i + " год является високосным");
+
+        float accumulation5 = 15_000.0f;
+        int month5 = 1;
+        while (accumulation5 < 12_000_000.0f) {
+            accumulation5 *= 1.07;
+            if (month5 % 6 == 0) {
+                System.out.printf("Месяц %d, сумма вклада %.2f\n", month5, accumulation5);
+            }
+            month5++;
         }
         System.out.println();
 
+
         System.out.println("Task 6");
-        for (int i = 7; i <= 98; i+=7) {
-            System.out.print(i + "\t");
+
+        float accumulation6 = 15_000.0f;
+        int month6 = 1;
+        int depositPeriod = 12 * 9;
+        int numberOfHalfYear = 1;
+        for (; month6 <= depositPeriod; month6++) {
+            accumulation6 *= 1.07;
+            if (month6 % 6 == 0) {
+                System.out.printf("полугодие %d, сумма вклада %.2f\n", numberOfHalfYear, accumulation6);
+                numberOfHalfYear++;
+            }
         }
-        System.out.print("\n\n");
+        System.out.println();
+
 
         System.out.println("Task 7");
-        for (int i = 2; i <= 512; i*=2) {
-            System.out.print(i + "\t");
+
+        int numberOfFirstFriday = 3;
+        int numberOfDayOfMonth = numberOfFirstFriday;
+//        for (; numberOfDayOfMonth <= 31; numberOfDayOfMonth++) {
+        while (numberOfDayOfMonth <= 31) {
+            System.out.printf("Сегодня пятница, %d-е число. Небходимо подготовить отчет.\n", numberOfDayOfMonth);
+            numberOfDayOfMonth += 7;
         }
-        System.out.print("\n\n");
+        System.out.println();
+
 
         System.out.println("Task 8");
-        int accumulation = 0;
-        for (int i = 1; i <=12; i++) {
-            accumulation+=29_000;
-            System.out.printf("Месяц %d, сумма накоплений равна %d рублей\n", i, accumulation);
-        }
-        System.out.print("\n");
 
-        System.out.println("Task 9");
-        float accumulation9 = 0.0f;
-        for (int i = 1; i <=12; i++) {
-            accumulation9 = (float) (accumulation9*1.01 + 29_000);
-            System.out.printf("Месяц %d, сумма накоплений равна %.2f рублей\n", i, accumulation9);
+        int actuallyYear = 2024;
+        int previousPeriod = actuallyYear - 200;
+        int nextPeriod = actuallyYear + 100;
+        int yearOfComet = 0;
+        while (yearOfComet <= nextPeriod) {
+            yearOfComet+=79;
+            if (yearOfComet >= previousPeriod && yearOfComet <= nextPeriod) {
+                System.out.println(yearOfComet);
+            }
         }
-        System.out.print("\n");        
 
-        System.out.println("Task 10");
-        for (int i = 1; i <= 10 ; i++) {
-            System.out.printf("2*%d=%d\n", i, 2*i);
-        }
     }
 }
